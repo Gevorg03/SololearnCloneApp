79bfcc0b67dda1fc4aa398e15aa32996c2dd57c5
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2023-02-19 12:01:17 +0400

    Delete app directory

--- app/src/main/java/com/example/sololearnclone/data/Course.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sololearnclone.data
-
-data class Course (
-    val courseImage: Int? = null,
-)
--- app/src/main/java/com/example/sololearnclone/data/Section.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sololearnclone.data
-
-import com.example.sololearnclone.R
-
-data class Section (
-    val sectionName: String? = null,
-    val sectionImage: Int? = R.drawable.ic_lock
-)
